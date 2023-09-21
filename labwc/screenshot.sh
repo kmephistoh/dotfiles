f08e5e466049e52a036fe73db97e472ee30cc13b
mephisto <kanhaosmile@gmail.com>
2023-09-21 13:57:21 +0800

    fix ksnip copy issue on wayland

--- labwc/screenshot.sh
+++ labwc/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 grim -g "$(slurp )" -t png $HOME/Pictures/grim-$NOW.png
-ksnip $HOME/Pictures/grim-$NOW.png
+QT_QPA_PLATFORM=xcb /usr/bin/ksnip $HOME/Pictures/grim-$NOW.png
