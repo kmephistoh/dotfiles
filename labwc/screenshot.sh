c5d2bd57d14f3257ad5da4f6d91b664ff1d3c6b6
mephisto <kanhaosmile@gmail.com>
2023-11-17 17:41:46 +0800

    minimize super h

--- labwc/screenshot.sh
+++ labwc/screenshot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 grim -g "$(slurp )" -t png $HOME/Pictures/grim-$NOW.png
-QT_QPA_PLATFORM=xcb /usr/bin/ksnip $HOME/Pictures/grim-$NOW.png
+#QT_QPA_PLATFORM=xcb /usr/bin/ksnip $HOME/Pictures/grim-$NOW.png
+/usr/bin/ksnip $HOME/Pictures/grim-$NOW.png
