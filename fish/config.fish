1902f50a2ef8d69ee5bc8f82fc8cb8328cbde679
mephisto <kanhaosmile@gmail.com>
2024-01-26 16:19:57 +0800

    fish use atuin search

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+   set -gx ATUIN_NOBIND "true"
+   atuin init fish | source
+   bind \cr _atuin_search
 end
 
 function fish_greeting
