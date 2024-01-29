af497a37331493b2925a4b0d9a08af7060cb137d
mephisto <kanhaosmile@gmail.com>
2024-01-29 11:18:35 +0800

    update fish abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -3,3 +3,5 @@ abbr -a cal "ncal -C"
 abbr -a md "docker run -d -p 8080:80 doocs/md:latest"
 abbr -a wshot 'grim -g "$(slurp)" - | swappy -f -'
 abbr -a wgif 'wf-recorder -f $(date +"%Y-%m-%d-%H%M%S").gif -c gif -g "$(slurp)"'
+abbr -a stock 'python /home/mephisto/xueqiu.py'
+abbr -a worklog 'python /home/mephisto/work/work_log.py'
