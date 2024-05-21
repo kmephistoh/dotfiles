cf78114aa8d38b928d2612c6caa7583a80eaffc2
mephisto <kanhaosmile@gmail.com>
2024-05-21 14:20:35 +0800

    emoji

--- gfw/gfw.sh
+++ gfw/gfw.sh
@@ -1,3 +1,3 @@
 sudo ip route del default
 sudo ip route add default via 192.168.18.190 dev wlp2s0
-sudo sed -i "s/192.168.18.1/129.168.18.190/g" /etc/resolv.conf
+sudo sed -i "s/192.168.18.1/192.168.18.190/g" /etc/resolv.conf
